9cbe66aac85916d092158e9ef0a03261115c7f24
Erick Pacheco <58119210+erick-pacheco@users.noreply.github.com>
2022-11-22 15:46:07 +0000

    Update king => kind

--- Main.java
+++ Main.java
@@ -2,7 +2,12 @@
 public class Main {
   public static void main(String args[]) {
 
+    Console console = new Console();
+    
     Truck icreamTruck = new Truck();
     icreamTruck.startShift();
+
+    console.log("What kind of ice cream would you like?");
+
   }
 }
